7f012ffcd7e57e905cfb9c7e8721084fc4ac98f4
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-07-03 15:18:32 -0300

    Fixed the bug showing the number of pages

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-class Filters{
-  pagination(items: any[],currentPage: number = 1,  pageSize: number = 20) {
+class Filters {
+  pagination(items: any[], currentPage: number = 1, pageSize: number = 20) {
     const AllItems: number = items.length
-    const maxPages:number = Math.round(items.length / pageSize)
-    // calculate total pages
-    let allPages:number = Math.ceil(AllItems / pageSize)
+    const maxPages: number = Math.round(items.length / pageSize)
 
+    // calcula o total de páginas
+    // calculate total pages
+    let allPages: number = Math.ceil(AllItems / pageSize)
+
+    // garante que a página atual não esteja fora do intervalo
     // ensure current page isn't out of range
     if (currentPage < 1) {
       currentPage = 1
@@ -14,48 +17,58 @@ class Filters{
 
     let startPage: number, endPage: number
     if (allPages <= maxPages) {
-      // total pages less than max so show all pages
+      // total de páginas menor que o máximo, então mostra todas as páginas
+      // all pages less than max so show all pages
       startPage = 1
       endPage = allPages
     } else {
-      // total pages more than max so calculate start and end pages
-      let maxPagesBeforeCurrentPage:number = Math.floor(maxPages / 2)
-      let maxPagesAfterCurrentPage:number = Math.ceil(maxPages / 2) - 1
+      // todas as páginas acima do máximo, calcula as páginas inicial e final
+      // all pages more than max so calculate start and end pages
+      let maxPagesBeforeCurrentPage: number = Math.floor(maxPages / 2)
+      let maxPagesAfterCurrentPage: number = Math.ceil(maxPages / 2) - 1
       if (currentPage <= maxPagesBeforeCurrentPage) {
-        // current page near the start
         startPage = 1
         endPage = maxPages
       } else if (currentPage + maxPagesAfterCurrentPage >= allPages) {
-        // current page near the end
         startPage = allPages - maxPages + 1
         endPage = allPages
       } else {
-        // current page somewhere in the middle
         startPage = currentPage - maxPagesBeforeCurrentPage
         endPage = currentPage + maxPagesAfterCurrentPage
       }
     }
-
+    // calcula os índices de item inicial e final
     // calculate start and end item indexes
-    let startIndex:number = (currentPage - 1) * pageSize
-    let endIndex:number = Math.min(startIndex + pageSize - 1, AllItems - 1)
+    let startIndex: number = (currentPage - 1) * pageSize
+    let endIndex: number = Math.min(startIndex + pageSize - 1, AllItems - 1)
 
+    // cria um array de páginas para repetir no controle de paginas
     // create an array of pages to ng-repeat in the pager control
-    let pages = Array.from(Array(endPage + 1 - startPage).keys()).map((i) => startPage + i)
+    let pages = Array.from(Array(endPage + 1 - startPage).keys()).map(
+      (i) => startPage + i
+    )
 
+    //realiza a paginação em si dos itens
+    // realiza uma paginação em si dos itens
     let contentItems: any[] = []
     if (currentPage == 1) {
       contentItems = items.slice(0, pageSize)
     } else {
-      const pageCall:number = currentPage - 1
-      contentItems = items.slice(pageSize * pageCall, pageSize + pageSize * pageCall)
+      const pageCall: number = currentPage - 1
+      contentItems = items.slice(
+        pageSize * pageCall,
+        pageSize + pageSize * pageCall
+      )
     }
+    // retorna o objeto com todas as propriedades exigidas paraa visualização
     // return object with all pager properties required by the view
+
+    console.log(pages.length)
     return {
       AllItems,
       currentPage,
       pageSize,
-      allPages,
+      allPages: allPages !== pages.length ? pages.length : allPages,
       startPage,
       endPage,
       startIndex,
