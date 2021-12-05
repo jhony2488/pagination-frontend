2a10d0c70c2d06be4879c98c26ceca4798ec9713
jhony2488 <jhon.araujo2488@gmail.com>
2021-12-05 12:43:21 -0300

    test: add test if items have a null value

--- __tests__/unit/Pagination.spec.ts
+++ __tests__/unit/Pagination.spec.ts
@@ -10,4 +10,7 @@ describe('Filters', () => {
   it('should perform the pagination returning the first 5 items, which are after the first 5 items of the array', () => {
     expect(Pagination.pagination(drones, 2, 5).items).toStrictEqual(dronesPagination2)
   })
+  it('should perform pagination returning 0 items if the items is null', () => {
+    expect(Pagination.pagination(null, 1, 5).items).toStrictEqual([])
+  })
 })
